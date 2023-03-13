3c61aaaeba3b2c4088522086cff6f3dbc5e2cba3
shreyastc3 <121991260+shreyastc3@users.noreply.github.com>
2023-03-13 10:02:03 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,8 +7,8 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u sibu123 -p $PASS
+docker login -u shreyas73 -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG sibu123/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG shreyas73/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push sibu123/$IMAGE:$BUILD_TAG
+docker push shreyas73/$IMAGE:$BUILD_TAG
