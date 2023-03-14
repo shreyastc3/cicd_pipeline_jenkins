cc109b2b9806eb917fc4a8e1d315de98c004ae88
shreyastc3 <121991260+shreyastc3@users.noreply.github.com>
2023-03-14 19:16:29 +0530

    updated ip address

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,4 +4,4 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth ec2-user@35.174.184.95:/tmp/.auth
+scp -i /opt/prod /tmp/.auth ec2-user@54.179.196.51:/tmp/.auth
